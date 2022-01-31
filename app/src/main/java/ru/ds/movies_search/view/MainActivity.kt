b986dfbca321f580ba2ba740f6428549a4028255
DmitrySergienko <dmitrisergienko@gmail.com>
2022-01-31 23:22:09 +0300

    Исправил ошибку (вышел в интернет через браузер, далее перенес код в другое app) вернул все как было.

--- app/src/main/java/ru/ds/movies_search/view/MainActivity.kt
+++ app/src/main/java/ru/ds/movies_search/view/MainActivity.kt
@@ -24,55 +24,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bind = MainActivityBinding.inflate(layoutInflater)
-        bind.ok.setOnClickListener(clickListener)
+
         setContentView(bind.root)
-        /*  if (savedInstanceState == null) {
-              supportFragmentManager.beginTransaction()
-                      .replace(R.id.container, MainFragment.newInstance())
-                      .commit()
-          }*/
-    }
-
-    private var clickListener: View.OnClickListener = object : View.OnClickListener {
-
-        @RequiresApi(Build.VERSION_CODES.N)
-        override fun onClick(v: View?) {
-            try {
-                val uri = URL(bind.url.text.toString())
-                val handler = Handler() //Запоминаем основной поток
-                Thread {
-                    var urlConnection: HttpsURLConnection? = null
-                    try {
-                        urlConnection = uri.openConnection() as HttpsURLConnection
-                        urlConnection.requestMethod =
-                            "GET" //установка метода получения данных — GET
-                        urlConnection.readTimeout = 10000 //установка таймаута — 10 000 миллисекунд
-                        val reader =
-                            BufferedReader(InputStreamReader(urlConnection.inputStream)) //читаем данные в поток
-                        val result = getLines(reader)
-
-                        // Возвращаемся к основному потоку
-                        handler.post {
-                            bind.webview.loadDataWithBaseURL(null, result, "text/html; charset=utf-8", "utf-8", null)
-                        }
-                    } catch (e: Exception) {
-                        Log.e("", "Fail connection", e)
-                        e.printStackTrace()
-                    } finally {
-                        urlConnection?.disconnect()
-                    }
-                }.start()
-            } catch (e: MalformedURLException) {
-                Log.e("", "Fail URI", e)
-                e.printStackTrace()
-            }
-        }
-
-        @RequiresApi(Build.VERSION_CODES.N)
-        private fun getLines(reader: BufferedReader): String {
-            return reader.lines().collect(Collectors.joining("\n"))
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commit()
         }
     }
+
+
 }
 
 
